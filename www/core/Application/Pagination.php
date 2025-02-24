6553ab272f4e75ec82cd782c0e965b32e445dc63
OksanaDenysenko <oksana.krivopisha@gmail.com>
2025-02-24 18:38:22 +0200

    added new route for paginaton

--- www/core/Application/Pagination.php
+++ www/core/Application/Pagination.php
@@ -6,20 +6,23 @@ use Exception;
 
 class Pagination
 {
-    protected int $countPages;
+    protected int $countPages; //загальна кількість сторінок
 
-    protected int $currentPage;
+    protected int $currentPage; // поточна сторінка
     protected string $uri; // посилання на сторінки
-    protected int $perPage = 1;
-    protected int $totalRecords;
+    protected int $perPage = 1; // кількість записів на сторінці
+    protected int $totalRecords; // загальна кількість записів
 
-    public function __construct($page, $perPage, $totalRecords)
+    /**
+     * @throws Exception
+     */
+    public function __construct(int $numberPage, int $perPage, int $totalRecords)
     {
         $this->perPage = $perPage;
         $this->totalRecords = $totalRecords;
         $this->countPages = $this->getCountPages();
-        $this->currentPage = $this->getCurrentPage($page);
-        $this->uri = getUri();
+        $this->currentPage = $this->getCurrentPage($numberPage);
+        $this->uri = $this->getUri();
 
     }
 
@@ -31,17 +34,31 @@ class Pagination
     /**
      * @throws Exception
      */
-    protected function getCurrentPage($page): int
+    protected function getCurrentPage(int $numberPage): int
     {
-        if ($page < 1 || $page > $this->countPages) {
+        if ($numberPage < 1 || $numberPage > $this->countPages) {
             throw new Exception(StatusCode::Page_Not_Found->name, StatusCode::Page_Not_Found->value);
         }
 
-        return $page;
+        return $numberPage;
     }
 
+    /**
+     * отримання позиції offset для sql-запиту
+     * @return int
+     */
     protected function getOffset(): int
     {
         return ($this->currentPage - 1) * $this->perPage;
+    }
+
+    private function getUri()
+    {
+        $uri=htmlspecialchars(strip_tags(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)));
+        show($uri);
+
+        if(!empty($uri)){
+
+        }
     }
 }
